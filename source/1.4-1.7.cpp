8e8ad16e31ad7064c445001246a3bcb92b275672
Monique Muehlner <monique.muehlner@t-online.de>
2020-05-09 16:56:13 +0200

    Initial commit

---
+++ source/1.4-1.7.cpp
@@ -0,0 +1,42 @@
+
+#Aufgabe 1.4
+#include <iostream>
+
+int main()
+{
+    
+}
+
+
+
+#Aufgabe 1.5
+int main()
+{
+    #Initialisierung setzt den Wert einer Variable, sobald sie erstellt wird(Speicher angefordert, evtl.Konstruktor aufgerufen)
+        int a = 325; // Initialisierung
+
+
+    #Zuweisung ist unter bestimmten Umständen nicht möglich, zum Beispiel wenn eine Variable const
+    #qualifiziert oder eine Referenz ist, oder wenn der Typ den Zuweisungsoperator nicht unterstützt.
+        int b;
+        b = 357;     // Zuweisung
+}
+
+
+
+#Aufgabe 1.6
+int main()
+{
+
+}
+
+
+
+
+#Aufgabe 1.7
+int main()
+{
+    #Signatur ist : Funktionsname und Parameter...
+    #ändert sich bei einer Signatur die Anzahl / Art der Parameter so spricht man von Übnerladung  
+    #d.h.wenn man mehr als eine Funktion mit dem gleichen namen, gleichen Rückgabewert aber unterschiedlicher Art von Parameter...
+}
