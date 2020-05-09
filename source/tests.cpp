8fb8e8cc104c0ebbf168b3fdb719575547d65da7
Monique Muehlner <monique.muehlner@t-online.de>
2020-05-09 13:55:35 +0200

    initial commit commit
    initial commit
    Initial commit

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,18 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include <cmath>
+
+int gcd(int a, int b)
+{
+    return 1;
+}
+
+TEST_CASE("describe_gcd", "[gcd]")
+{
+    REQUIRE(gcd(2,4) == 2);
+    REQUIRE(gcd(9,6) == 3);
+    REQUIRE(gcd(3,7) == 1);
+}
 
 int main(int argc, char* argv[])
 {
