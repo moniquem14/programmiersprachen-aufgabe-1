e4c4619fabe0833c93ea1cdfa188694e7be9a8b1
Monique Muehlner <monique.muehlner@t-online.de>
2020-05-11 11:25:06 +0200

    Initial commit

--- source/1.12.cpp
+++ source/1.12.cpp
@@ -27,16 +27,16 @@ float berech_v(float h, float r)
 
 TEST_CASE("describe_berech_o", "[berech_o]")
 {
-    REQUIRE(berech(6, 9) == );
-    REQUIRE(berech(13, 9) == );
-    REQUIRE(berech(3, 24) == );
+    REQUIRE(berech(6, 9) == 848.23);
+    REQUIRE(berech(13, 9) == 1244.071);
+    REQUIRE(berech(3, 24) == 4071.504);
 }
 
 TEST_CASE("describe_berech_v", "[berech_v]")
 {
-    REQUIRE(berech(6, 9) == );
-    REQUIRE(berech(13, 9) == );
-    REQUIRE(berech(3, 24) == );
+    REQUIRE(berech(6, 9) == 1526.814);
+    REQUIRE(berech(13, 9) == 3308.097);
+    REQUIRE(berech(3, 24) == 5428.672);
 }
 
 int main(int argc, char* argv[])
