51b2f5721b8ee2a78fe8b9819d13fac40c95d24a
Monique Muehlner <monique.muehlner@t-online.de>
2020-05-11 14:38:03 +0200

    Initial commit

---
+++ source/1.11.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+#include <cmath>
+
+double fract(double a)
+{
+	double i;
+	double f = modf(a, &i);
+	std::cout << f;
+	return f;
+}
+
+TEST_CASE("describe_fract", "[fract]")
+{
+	REQUIRE(0.45 == Approx(3.45));
+	REQUIRE(0.79 == Approx(6.79));
+	REQUIRE(0.145 == Approx(7.145));
+}
+
+int main(int argc, char* argv[])
+{
+	return Catch::Session().run(argc, argv);
+}
